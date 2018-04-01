53f4a063db74a97e01c2f1677a55abbe6df4bf09
l7ssha <szymon.uglis@hotmail.com>
2018-04-01 09:25:42 +0200

    Moved token to Responser

--- src/main/java/pl/l7ssha/javasteam/CsgoService.java
+++ src/main/java/pl/l7ssha/javasteam/CsgoService.java
@@ -13,21 +13,16 @@ import pl.l7ssha.javasteam.csgo.models.serverstatus.ServerStatus;
 import pl.l7ssha.javasteam.utils.Responser;
 
 public class CsgoService {
-    private String token = null;
-    private final String serverStatusUrl = "https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key=%s";
-    private final String mapPlaytimeUrl = "https://api.steampowered.com/ICSGOServers_730/GetGameMapsPlaytime/v1/?key=%s&format=json&interval=%s&gamemode=%s&mapgroup=operation";
+    private final String serverStatusUrl = "https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key=";
+    private final String mapPlaytimeUrl = "https://api.steampowered.com/ICSGOServers_730/GetGameMapsPlaytime/v1/?format=json&interval=%s&gamemode=%s&mapgroup=operation&key=";
 
-    CsgoService(String token) {
-        this.token = token;
-    }
-
-    private CsgoService() { }
+    CsgoService() { }
 
     public ServerStatus getGameServerStatus() throws Exception {
-        return (ServerStatus) Responser.getResponse(String.format(serverStatusUrl, token), ServerStatus.class);
+        return (ServerStatus) Responser.getResponse(serverStatusUrl, ServerStatus.class);
     }
 
     public MapPlaytime getMapPlaytime(Gamemode mode, Interval interval) throws Exception {
-        return (MapPlaytime) Responser.getResponse(String.format(mapPlaytimeUrl, token, interval.toString(), mode.toString()), MapPlaytime.class);
+        return (MapPlaytime) Responser.getResponse(String.format(mapPlaytimeUrl, interval.toString(), mode.toString()), MapPlaytime.class);
     }
 }
--- src/main/java/pl/l7ssha/javasteam/utils/Responser.java
+++ src/main/java/pl/l7ssha/javasteam/utils/Responser.java
@@ -9,22 +9,40 @@ package pl.l7ssha.javasteam.utils;
 import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import pl.l7ssha.javasteam.csgo.MapsPlaytimeDeserializer;
 import pl.l7ssha.javasteam.csgo.ServerStatusDeserializer;
 import pl.l7ssha.javasteam.csgo.models.mapsplaytime.MapPlaytime;
 import pl.l7ssha.javasteam.csgo.models.serverstatus.ServerStatus;
+import pl.l7ssha.javasteam.steamuser.FriendListDeserializer;
+import pl.l7ssha.javasteam.steamuser.UserBansDeserializer;
+import pl.l7ssha.javasteam.steamuser.models.FriendListNode;
+import pl.l7ssha.javasteam.steamuser.models.UserBans;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.List;
 
 public class Responser {
     private static Gson gson = new GsonBuilder()
             .registerTypeAdapter(ServerStatus.class, new ServerStatusDeserializer())
             .registerTypeAdapter(MapPlaytime.class, new MapsPlaytimeDeserializer())
+            .registerTypeAdapter(new TypeToken<List<FriendListNode>>() { }.getType(), new FriendListDeserializer())
+            .registerTypeAdapter(UserBans.class, new UserBansDeserializer())
             .create();
 
+    private static String token = "";
+
+    public static void setToken(String token) {
+        Responser.token = token;
+    }
+
+    public static Gson getGson() {
+        return gson;
+    }
+
     public static Object getResponse(String url, Type type) {
-        HttpRequest req = HttpRequest.get(url);
+        HttpRequest req = HttpRequest.get((url + token));
 
         if(req.code() != 200) {
             throw new HttpRequest.HttpRequestException(new IOException("Server returned with code " + req.code() + ", with message: "+ req.body()));
