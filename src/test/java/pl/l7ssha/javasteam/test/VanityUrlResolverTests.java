167caaa903e5e9c7892ac54db971d239d11ea4d7
l7ssha <szymon.uglis@hotmail.com>
2018-04-03 10:45:33 +0200

    Some VanityUrl tests

---
+++ src/test/java/pl/l7ssha/javasteam/test/VanityUrlResolverTests.java
@@ -0,0 +1,37 @@
+package pl.l7ssha.javasteam.test;
+
+// pl.l7ssha.javasteam.test
+//
+// Date created: 03 Apr 2018
+// Author: Szymon 'l7ssha' Uglis
+// Free for open source use, all changes send back to author
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pl.l7ssha.javasteam.SteamAPI;
+import pl.l7ssha.javasteam.SteamUserService;
+import pl.l7ssha.javasteam.steamuser.ISteamUser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class VanityUrlResolverTests {
+    static SteamUserService usrApi;
+
+    @BeforeAll
+    static void getToken() throws IOException {
+        String token = Files.readAllLines(Paths.get("token.txt")).get(0);
+
+        usrApi = new SteamAPI(token).getSteamUserService();
+    }
+
+    @Test
+    void vanityResolvTest() {
+        ISteamUser user = usrApi.getSteamUser("l7ssha");
+
+        assertNotNull(user.getUserBans().getDaysSinceLastBan());
+    }
+}
