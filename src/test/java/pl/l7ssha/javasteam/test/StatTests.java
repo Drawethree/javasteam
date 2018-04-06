fbf2234907b2b496d829e1e53686960dc48ba816
l7ssha <szymon.uglis@hotmail.com>
2018-04-06 19:11:35 +0200

    Test for global achievements endpoint

--- src/test/java/pl/l7ssha/javasteam/test/StatTests.java
+++ src/test/java/pl/l7ssha/javasteam/test/StatTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import pl.l7ssha.javasteam.SteamAPI;
 import pl.l7ssha.javasteam.SteamStatsService;
 import pl.l7ssha.javasteam.schema.GameSchema;
+import pl.l7ssha.javasteam.steamstats.AchievementsGlobalPercentages;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 // pl.l7ssha.javasteam.test
 //
@@ -33,5 +35,21 @@ public class StatTests {
         assertNotNull(csgoSchema.getGameVersion());
         assertNotNull(csgoSchema.getAchievements().get(1).getIconGray());
         assertNotNull(csgoSchema.getStats().get(10).getDisplayName());
+    }
+
+    @Test
+    void globalAchievementsWithoutDescription() {
+        AchievementsGlobalPercentages percentages = statsService.getGlobalAchievementsPercentages("730");
+
+        assertNotNull(percentages.getAchievements().get(10).getName());
+        assertNull(percentages.getAchievements().get(5).getDescription());
+    }
+
+    @Test
+    void globalAchievementsWithDescription() {
+        AchievementsGlobalPercentages percentages = statsService.getGlobalAchievementsPercentagesWithDescription("730");
+
+        assertNotNull(percentages.getAchievements().get(10).getName());
+        assertNotNull(percentages.getAchievements().get(5).getDescription());
     }
 }
