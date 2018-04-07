09e2270e7b580ae0d663518a980dfd0ec4d8dc50
l7ssha <szymon.uglis@hotmail.com>
2018-04-07 14:22:31 +0200

    Sorted variables

--- src/main/java/pl/l7ssha/javasteam/utils/Links.java
+++ src/main/java/pl/l7ssha/javasteam/utils/Links.java
@@ -12,6 +12,7 @@ public class Links {
     public static final String friendListUrl = "https://api.steampowered.com/ISteamUser/getFriendList/v1/?steamid=%s&key=";
     public static final String userBansUrl = "https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?format=json&steamids=%s&key=";
     public static final String userSummaryUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?format=json&steamids=%s&key=";
+    public static final String playerAchievementsUrl = "https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?steamid=%s&appid=%s&l=ENG&key=";
 
     // CSGO
     public static final String serverStatusUrl = "https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key=";
@@ -22,21 +23,14 @@ public class Links {
     public static final String shopFeaturedAppsUrl = "https://store.steampowered.com/api/featured/?key=";
     public static final String shopFeaturedCategoriesUrl = "https://store.steampowered.com/api/featuredcategories/?key=";
     public static final String shopPackageDetailsUrl = "https://store.steampowered.com/api/packagedetails/?packageids=%s&key=";
+    public static final String storeSearching = "https://api.steampowered.com/IStoreService/GetAppList/v1/?format=json&if_modified_since=%s&have_description_language=%s&include_games=%s&include_dlc=%s&include_hardware=%s&include_software=%s&include_videos=%s&last_appid=%s&max_results=%s&key=";
+    public static final String newsForApp = "https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?format=json&appid=%s&count=%s&key=";
 
-    // Current players
+    // Stats
+    public static final String appSchemaUrl = "https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?appid=%s&l=%s&key=";
+    public static final String globalAchievementsUrl = "https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/?gameid=%s&format=json&key=";
     public static final String currentPlayersUrl = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=%s&format=json&key=";
 
     // RESOLVE VANITY URL
     public static final String vanityUrl = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?format=json&vanityurl=%s&url_type=%s&key=";
-
-    // Store
-    public static final String storeSearching =
-            "https://api.steampowered.com/IStoreService/GetAppList/v1/?format=json&if_modified_since=%s&have_description_language=%s&include_games=%s&include_dlc=%s&include_hardware=%s&include_software=%s&include_videos=%s&last_appid=%s&max_results=%s&key=";
-    public static final String newsForApp = "https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?format=json&appid=%s&count=%s&key=";
-
-    public static final String appSchemaUrl = "https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?appid=%s&l=%s&key=";
-
-    public static final String playerAchievementsUrl = "https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?steamid=%s&appid=%s&l=ENG&key=";
-
-    public static final String globalAchievementsUrl = "https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/?gameid=%s&format=json&key=";
 }
