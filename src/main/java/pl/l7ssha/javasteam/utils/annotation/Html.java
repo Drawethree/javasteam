02122ec5d969d7b9961df906127b6c8e1d4431b5
l7ssha <szymon.uglis@hotmail.com>
2018-04-07 14:51:32 +0200

    Add @Html for html content marking

---
+++ src/main/java/pl/l7ssha/javasteam/utils/annotation/Html.java
@@ -0,0 +1,12 @@
+package pl.l7ssha.javasteam.utils.annotation;
+
+// pl.l7ssha.javasteam.utils.annotation
+//
+// Date created: 06 Apr 2018
+// Author: Szymon 'l7ssha' Uglis
+// Free for open source use, all changes send back to author
+
+/**
+ * Object annotated has or returns value with HTML Tags
+ */
+public @interface Html {}
