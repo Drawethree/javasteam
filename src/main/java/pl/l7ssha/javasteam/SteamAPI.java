46daaa9ed96af9548166e4f2dd667498e332e865
l7ssha <szymon.uglis@hotmail.com>
2018-04-10 20:39:30 +0200

    First javadoc

--- src/main/java/pl/l7ssha/javasteam/SteamAPI.java
+++ src/main/java/pl/l7ssha/javasteam/SteamAPI.java
@@ -13,22 +13,43 @@ public class SteamAPI {
         ResponserUtils.setToken(key);
     }
 
+    /**
+     * Provides access to CSGO-specific endpoints. No marketplace included.
+     * @return CsgoService
+     */
     public CsgoService getCsgoService() {
         return new CsgoService();
     }
 
+    /**
+     * Provides access to user-related endpoints of steam API.
+     * @return SteamService
+     */
     public SteamUserService getSteamUserService() {
         return new SteamUserService();
     }
 
+    /**
+     * Provides access to endpoints which relates to steam store
+     * @return StoreFrontService
+     */
     public StoreFrontService getStoreFront() {
         return new StoreFrontService();
     }
 
+
+    /**
+     * Provides ability to resolve steamID based on nick or name
+     * @return VanityUrlResolver
+     */
     public VanityUrlResolver getVanityUrlResolver() {
         return new VanityUrlResolver();
     }
 
+    /**
+     * Provides ability to acces steam stats services.
+     * @return SteamStatsService
+     */
     public SteamStatsService getStatService() {
         return new SteamStatsService();
     }
