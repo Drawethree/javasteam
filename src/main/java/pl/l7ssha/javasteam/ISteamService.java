3d906268a9e2dd288529ab96433bac0315c11d92
l7ssha <szymon.uglis@hotmail.com>
2018-04-18 17:39:45 +0200

    Added ISteamService

---
+++ src/main/java/pl/l7ssha/javasteam/ISteamService.java
@@ -0,0 +1,9 @@
+package pl.l7ssha.javasteam;
+
+// pl.l7ssha.javasteam
+//
+// Date created: 18 Apr 2018
+// Author: Szymon 'l7ssha' Uglis
+// Free for open source use, all changes send back to author
+
+public interface ISteamService {}
