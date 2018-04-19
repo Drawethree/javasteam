69924b7e7b481d0cd88fa3f4e5d69823ff3c0dff
l7ssha <szymon.uglis@hotmail.com>
2018-04-19 18:28:21 +0200

    Convert obj to game

---
+++ src/main/java/pl/l7ssha/javasteam/storefront/Gameable.java
@@ -0,0 +1,11 @@
+package pl.l7ssha.javasteam.storefront;
+
+// pl.l7ssha.javasteam.storefront
+//
+// Date created: 19 Apr 2018
+// Author: Szymon 'l7ssha' Uglis
+// Free for open source use, all changes send back to author
+
+public interface Gameable {
+    SteamGame toSteamGame();
+}
