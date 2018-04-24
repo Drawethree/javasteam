67052160068d73ea8e6242a72d65b52e842505d3
l7ssha <szymon.uglis@hotmail.com>
2018-04-24 20:06:37 +0200

    Fix typo in class name

---
+++ api/src/main/java/pl/l7ssha/javasteam/storefront/steamgame/Requirements.java
@@ -0,0 +1,21 @@
+package pl.l7ssha.javasteam.storefront.steamgame;
+
+// pl.l7ssha.javasteam.storefront.models.steamgame
+//
+// Date created: 01 Apr 2018
+// Author: Szymon 'l7ssha' Uglis
+
+public class Requirements {
+    private String minimum;
+    private String recommended;
+
+    public Requirements() { }
+
+    public String getMinimum() {
+        return minimum;
+    }
+
+    public String getRecommended() {
+        return recommended;
+    }
+}
