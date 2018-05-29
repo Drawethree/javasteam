b02f57afe48d3f4ed1457025e77355c891b04908
Szymon Uglis <szymon.uglis@tuta.io>
2018-05-29 15:59:23 +0200

    Implemented iterable

--- src/main/java/pl/l7ssha/javasteam/storefront/steamgame/achievements/Achievements.java
+++ src/main/java/pl/l7ssha/javasteam/storefront/steamgame/achievements/Achievements.java
@@ -1,5 +1,6 @@
 package pl.l7ssha.javasteam.storefront.steamgame.achievements;
 
+import java.util.Iterator;
 import java.util.List;
 
 // pl.l7ssha.javasteam.storefront.models.steamgame.achievements
@@ -7,17 +8,30 @@ import java.util.List;
 // Date created: 01 Apr 2018
 // Author: Szymon 'l7ssha' Uglis
 
-public class Achievements {
+public class Achievements implements Iterable<Achievement> {
     private int total;
     private List<Achievement> highlighted;
 
     public Achievements() { }
 
+    /**
+     * Total number of game achievements
+     * @return Number of achievements
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * List of Achievements
+     * @return List of achievements
+     */
     public List<Achievement> getHighlighted() {
         return highlighted;
+    }
+
+    @Override
+    public Iterator<Achievement> iterator() {
+        return highlighted.iterator();
     }
 }
